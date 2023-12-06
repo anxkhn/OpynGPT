8fb64276c9bde91a79e1107fdff4a4e177706ea8
= <anaskhaaan.28@gmail.com>
2023-12-06 12:14:59 +0530

    rename to OpynGPT ;

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# FreeGPTpy
+# OpynGPT
 
-FreeGPTpy is a Python module that facilitates interactions with the OpenAI GPT-3.5 Turbo API to generate natural language responses based on user input. Notably, FreeGPTpy operates without requiring authentication or API keys, providing a straightforward and accessible tool for natural language generation.
+OpynGPT is a Python module that facilitates interactions with the OpenAI GPT-3.5 Turbo API to generate natural language responses based on user input. Notably, OpynGPT operates without requiring authentication or API keys, providing a straightforward and accessible tool for natural language generation.
 
 ## Prerequisites
 
-Before using FreeGPTpy, ensure you have the following:
+Before using OpynGPT, ensure you have the following:
 
 - Python 3.x installed
 - The `requests` library installed. You can install it using:
@@ -15,18 +15,18 @@ Before using FreeGPTpy, ensure you have the following:
 
 ## Installation
 
-You can install FreeGPTpy using pip:
+You can install OpynGPT using pip:
 
 ```bash
-pip install freegptpy
+pip install opyngpt
 ```
 
 ## Usage
 
-1. Import the `prompt` function from FreeGPTpy:
+1. Import the `prompt` function from OpynGPT:
 
    ```python
-   from freegptpy import prompt
+   from opyngpt import prompt
    ```
 
 2. Use the `prompt` function to generate natural language responses:
@@ -42,11 +42,11 @@ pip install freegptpy
 
 - Sends user input to the GPT-3.5 Turbo API and receives natural language responses.
 - No authentication or API keys required, making it hassle-free to use.
-- Inspired by [tgpt](https://github.com/aandrew-me/tgpt/), FreeGPTpy utilizes a pool of users' data and the API from fakeopen.com.
+- Inspired by [tgpt](https://github.com/aandrew-me/tgpt/), OpynGPT utilizes a pool of users' data and the API from fakeopen.com.
 
 ## Contributing
 
-If you'd like to contribute to FreeGPTpy, feel free to fork the repository and submit a pull request.
+If you'd like to contribute to OpynGPT, feel free to fork the repository and submit a pull request.
 
 ## License
 
--- setup.py
+++ setup.py
@@ -1,10 +1,66 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='freegptpy',
+    name='opyngpt',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
         'requests',
     ],
+    description='Python module for interacting with the OpenAI GPT-3.5 Turbo API without authentication.',
+    long_description="""
+    OpynGPT is a Python module that facilitates interactions with the OpenAI GPT-3.5 Turbo API to generate natural language responses based on user input. Notably, OpynGPT operates without requiring authentication or API keys, providing a straightforward and accessible tool for natural language generation.
+
+    ## Prerequisites
+
+    Before using OpynGPT, ensure you have the following:
+
+    - Python 3.x installed
+    - The `requests` library installed. You can install it using:
+
+    ```bash
+    pip install requests
+    ```
+
+    ## Installation
+
+    You can install OpynGPT using pip:
+
+    ```bash
+    pip install opyngpt
+    ```
+
+    ## Usage
+
+    1. Import the `prompt` function from OpynGPT:
+
+    ```python
+    from opyngpt import prompt
+    ```
+
+    2. Use the `prompt` function to generate natural language responses:
+
+    ```python
+    user_input = "Write a factorial code in python."
+    response = prompt(user_input)
+
+    print(response)
+    ```
+
+    ## Features
+
+    - Sends user input to the GPT-3.5 Turbo API and receives natural language responses.
+    - No authentication or API keys required, making it hassle-free to use.
+    - Inspired by [tgpt](https://github.com/aandrew-me/tgpt/), OpynGPT utilizes a pool of users' data and the API from fakeopen.com.
+
+    ## Contributing
+
+    If you'd like to contribute to OpynGPT, feel free to fork the repository and submit a pull request.
+
+    ## License
+
+    This project is licensed under the GPLv3 License - check out [this website](https://www.tldrlegal.com/license/gnu-general-public-license-v3-gpl-3) for more information.
+    """,
+    long_description_content_type='text/markdown',
+    author='Anas Khan',
 )
