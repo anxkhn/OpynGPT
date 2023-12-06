2d5494da11a1242894bb498e8b1dd396ab5d6e03
= <anaskhaaan.28@gmail.com>
2023-12-06 12:22:13 +0530

    final version

--- setup.py
+++ setup.py
@@ -2,67 +2,76 @@ from setuptools import setup, find_packages
 
 setup(
     name='opyngpt',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[
         'requests',
     ],
     description='Python module for interacting with the OpenAI GPT-3.5 Turbo API without authentication.',
     long_description="""
-    OpynGPT is a Python module that facilitates interactions with the OpenAI GPT-3.5 Turbo API to generate natural language responses based on user input. Notably, OpynGPT operates without requiring authentication or API keys, providing a straightforward and accessible tool for natural language generation.
+OpynGPT is a Python module that facilitates interactions with the OpenAI GPT-3.5 Turbo API to generate natural language responses based on user input. Notably, OpynGPT operates without requiring authentication or API keys, providing a straightforward and accessible tool for natural language generation.
 
-    ## Prerequisites
+Prerequisites
+-------------
 
-    Before using OpynGPT, ensure you have the following:
+Before using OpynGPT, ensure you have the following:
 
-    - Python 3.x installed
-    - The `requests` library installed. You can install it using:
+- Python 3.x installed
+- The `requests` library installed. You can install it using:
 
-    ```bash
+  ::
+  
     pip install requests
-    ```
 
-    ## Installation
+Installation
+------------
 
-    You can install OpynGPT using pip:
+You can install OpynGPT using pip:
 
-    ```bash
-    pip install opyngpt
-    ```
+::
 
-    ## Usage
+  pip install opyngpt
 
-    1. Import the `prompt` function from OpynGPT:
+Usage
+-----
 
-    ```python
-    from opyngpt import prompt
-    ```
+1. Import the `prompt` function from OpynGPT:
 
-    2. Use the `prompt` function to generate natural language responses:
+::
 
-    ```python
-    user_input = "Write a factorial code in python."
-    response = prompt(user_input)
+  from opyngpt import prompt
 
-    print(response)
-    ```
+2. Use the `prompt` function to generate natural language responses:
 
-    ## Features
+::
 
-    - Sends user input to the GPT-3.5 Turbo API and receives natural language responses.
-    - No authentication or API keys required, making it hassle-free to use.
-    - Inspired by [tgpt](https://github.com/aandrew-me/tgpt/), OpynGPT utilizes a pool of users' data and the API from fakeopen.com.
+  user_input = "Write a factorial code in python."
+  response = prompt(user_input)
 
-    ## Contributing
+  print(response)
 
-    If you'd like to contribute to OpynGPT, feel free to fork the repository and submit a pull request.
+Features
+--------
 
-    ## License
+- Sends user input to the GPT-3.5 Turbo API and receives natural language responses.
+- No authentication or API keys required, making it hassle-free to use.
+- Inspired by [tgpt](https://github.com/aandrew-me/tgpt/), OpynGPT utilizes a pool of users' data and the API from fakeopen.com.
 
-    This project is licensed under the GPLv3 License - check out [this website](https://www.tldrlegal.com/license/gnu-general-public-license-v3-gpl-3) for more information.
+Contributing
+------------
 
-    [GitHub Repository](https://github.com/anxkhn/OpynGPT)
-    """,
-    long_description_content_type='text/markdown',
+If you'd like to contribute to OpynGPT, feel free to fork the repository and submit a pull request.
+
+License
+-------
+
+This project is licensed under the GPLv3 License - check out [this website](https://www.tldrlegal.com/license/gnu-general-public-license-v3-gpl-3) for more information.
+
+GitHub Repository
+-----------------
+
+`GitHub Repository <https://github.com/anxkhn/OpynGPT>`_
+""",
+    long_description_content_type='text/x-rst',
     author='Anas Khan',
 )
